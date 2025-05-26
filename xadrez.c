3217f58d477a6ee65536c009737285a2da444ed3
Vitor Hugo <110561757+VitorHugoTrajano@users.noreply.github.com>
2025-05-26 21:01:05 +0000

    Nível novato conclu

--- xadrez.c
+++ xadrez.c
@@ -4,16 +4,43 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+int main(){
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int opcao;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("Bem-Vindo ao início do nosso jogo de Xadrez.\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    do {
+        printf("Menu Principal.\n");
+        printf("1. Bispo, o Bispo andará 5 casas na diagonal para direita.\n");
+        printf("2. Torre, a Torre andará 5 casas a direita.\n");
+        printf("3. Rainha, a Rainha andará 8 casas a esquerda.\n");
+        printf("4. Sair do programa.\n");
+        printf("Escolha uma das opções: ");
+        scanf("%d", &opcao);
 
+        
+        if(opcao == 1){// Implementação de Movimentação do Bispo
+            for(int i = 1; i <= 5; i++) {
+                printf("%d° casa para cima.\n", i);
+                printf("%d° casa para direita.\n", i);
+            }
+        } else if(opcao == 2) {// Implementação de Movimentação do Torre
+            for(int i = 1; i <= 5; i ++) {
+                printf("%d° casa a direita.\n", i);
+            }
+        } else if(opcao == 3) {// Implementação de Movimentação do Rainha
+            for(int i = 1; i <= 8; i ++) {
+                printf("%d° casa a esquerda.\n", i);
+            }
+        }else if(opcao == 4){
+            printf("Finalizando o programa...\n");
+        }else {
+            printf("Opção inválida, o programa será finalizado...");
+        }
+    }while(opcao >= 1 && opcao <= 3);
+    
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
